944e1381bd81a9ed58f3895b40c24eea8c07a614
Fernando-Onishi <onishi.fernando2306@gmail.com>
2025-08-14 09:56:24 -0300

    Add files via upload

--- Lista02/src/Lista02.java
+++ Lista02/src/Lista02.java
@@ -17,9 +17,50 @@ public class Lista02 {
 		double sub = num1 - num2;
 		double mult = num1 * num2;
 		double div = num1 / num2;
-		System.out.println("A soma do seu número é: "+ soma + "\nA subtração do número é: "+sub+"\nA multiplicação é: "+mult+"\nA divisão é: "+div);
+		System.out.println("A soma do seu número é: "+ soma + "\n A subtração do número é: "+sub+"\nA multiplicação é: "+mult+"\nA divisão é: "+div);
 		
 		//2
+		double tempC;
+		System.out.print("Informe a temperatura em graus Celsius: ");
+		tempC = scanner.nextDouble();
+		double F = (tempC * 1.8) + 32;
+		System.out.println("A conversão da temperatura em graus Fahrenheit: "+F+" graus Fahrenheit");
+		
+		//3
+		double real;
+		double dolar = 5.70;
+		System.out.print("Digite seu valor em real e receberá a conversão para dolár: ");
+		real = scanner.nextDouble();
+		double conversor = real / dolar;
+		System.out.println("Seu valor em dolár é: $"+conversor);
+		
+		//4
+		double altura;
+		double base;
+		System.out.println("Digite as informações pedidas que a área no final será exibida.");
+		System.out.print("Informe a base do retângulo: ");
+		base = scanner.nextDouble();
+		System.out.print("Infomre a altura do retângulo: ");
+		altura = scanner.nextDouble();
+		double area = base * altura;
+		System.out.print("A área do retângulo é: " + area);
+		
+		//5
+		int idade;
+		System.out.print("Informe sua idade: ");
+		idade = scanner.nextInt();
+		double meses = idade * 12;
+		System.out.print("Você viveu "+meses+" meses");
+		
+		//6
+		double minutos;
+		System.out.print("Informe o tempo em minutos: ");
+		
+		
+		
+		
+		
+		
 		
 		
 
